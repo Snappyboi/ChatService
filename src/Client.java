794d21d7f1f4c2d7c3f6485960dfb4d5ebedc3b7
Chad Tiffner <63892663+Snappyboi@users.noreply.github.com>
2024-02-09 13:29:33 -0500

    Framed files

---
+++ src/Client.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+public class Client {
+    public static void main(String[] args){
+
+    }
+    public static void handleServer(){
+
+    }
+    public static void disconnect(){
+
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,14 @@
+import java.net.*;
+import java.util.*;
+import java.io.*;
+public class Server{
+    public static void main(String[] args){
+
+    }
+    public static void handleClient(){
+
+    }
+    public static void disconnect(){
+        
+    }
+}
