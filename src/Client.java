2c2ba6f2a07bf11287ae27f2f0994bc035c993ba
Snappyboi <63892663+Snappyboi@users.noreply.github.com>
2024-02-09 15:45:09 -0500

    almost working chatroom

--- src/Client.java
+++ src/Client.java
@@ -2,11 +2,48 @@ import java.net.*;
 import java.util.*;
 import java.io.*;
 public class Client {
+    private static InetAddress host;
+    private static final int PORT = 8080; 
+    private static Socket link;
     public static void main(String[] args){
-
+        try{
+            host = InetAddress.getLocalHost();
+        }
+        catch(UnknownHostException uhEx){
+            System.out.println("Host ID not found!");
+            System.exit(1);
+        }
+        accessServer();
     }
-    public static void handleServer(){
+    public static void accessServer(){
+        link = null;
+        try{
+            link = new Socket(host, PORT);
 
+            Scanner serverMessage = new Scanner(link.getInputStream());
+            PrintWriter output = new PrintWriter(link.getOutputStream(), true);
+            Scanner userInput = new Scanner(System.in);
+            String inMessage = "";
+            String outMessage = "";
+            System.out.println("Welcome to the chatroom.");
+
+            while(!inMessage.equals("***END***")){
+                if(serverMessage.hasNextLine()){
+                    inMessage = serverMessage.nextLine();
+                    System.out.println("SERVER> " + inMessage);
+                }
+                //if(userInput.hasNextLine()){
+                    outMessage = userInput.nextLine();
+                    output.println(outMessage);
+                //}
+            }
+        }
+        catch(IOException ioEx){
+            ioEx.printStackTrace();
+        }
+        finally{
+            disconnect();
+        }
     }
     public static void disconnect(){
 
