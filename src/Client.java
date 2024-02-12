da2c0824cb5cb44a3e08187de06f3c4e75745fa5
Snappyboi <63892663+Snappyboi@users.noreply.github.com>
2024-02-11 23:50:57 -0500

    Update Client.java
    completely changed this, now just gotta make it work

--- src/Client.java
+++ src/Client.java
@@ -1,48 +1,94 @@
 import java.net.*;
 import java.util.*;
 import java.io.*;
-public class Client {
+public class Client extends Thread {
     private static InetAddress host;
     private static final int PORT = 8080; 
     private static Socket link;
     public static void main(String[] args){
-        try{
-            host = InetAddress.getLocalHost();
+        Scanner sc = new Scanner(System.in);
+        int listenPort;
+        int targetPort;
+        String ipAddress;
+        System.out.println("Enter port to listen: ");
+        
+        while(!sc.hasNextInt()){
+            System.out.println("Invalid input. Enter a valid port number.");
+            System.out.println("Enter port: ");
+            sc.next();
         }
-        catch(UnknownHostException uhEx){
-            System.out.println("Host ID not found!");
-            System.exit(1);
+        listenPort = sc.nextInt();
+        sc.nextLine(); //consume empty line
+
+        System.out.println("Enter target IP Address: ");
+        ipAddress = sc.nextLine();
+        System.out.println("Enter target port: ");
+        while(!sc.hasNextInt()){
+            System.out.println("Invalid input. Enter a valid port number.");
+            System.out.println("Enter port: ");
+            sc.next();
         }
-        accessServer();
+        targetPort = sc.nextInt();
+
+        new Thread(() -> startServer(listenPort)).start();
+        startClient(ipAddress, targetPort);
     }
-    public static void accessServer(){
-        link = null;
-        try{
-            link = new Socket(host, PORT);
+    public static void startServer(int listenPort){
+        try(ServerSocket serverSocket = new ServerSocket(listenPort)){
+            System.out.println("Opening port...");
 
-            Scanner serverMessage = new Scanner(link.getInputStream());
-            PrintWriter output = new PrintWriter(link.getOutputStream(), true);
-            Scanner userInput = new Scanner(System.in);
-            String inMessage = "";
-            String outMessage = "";
-            System.out.println("Welcome to the chatroom.");
+            while(true){
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Connected to: " + clientSocket.getInetAddress());
 
-            while(!inMessage.equals("***END***")){
-                if(serverMessage.hasNextLine()){
-                    inMessage = serverMessage.nextLine();
-                    System.out.println("SERVER> " + inMessage);
-                }
-                //if(userInput.hasNextLine()){
-                    outMessage = userInput.nextLine();
-                    output.println(outMessage);
-                //}
+                new Thread(() -> handleChat(clientSocket)).start();
             }
         }
         catch(IOException ioEx){
             ioEx.printStackTrace();
         }
-        finally{
-            disconnect();
+    }
+    public static void startClient(String targetAddress, int targetPort){
+        try(
+            Socket targetSocket = new Socket(targetAddress, targetPort);
+            BufferedReader input = new BufferedReader(new InputStreamReader(targetSocket.getInputStream()));
+            PrintWriter output = new PrintWriter(targetSocket.getOutputStream());
+            Scanner keyboard = new Scanner(System.in);
+        ){
+            System.out.println("Connected to " + targetSocket.getInetAddress() + ". Type ***EXIT*** to exit.");
+
+            new Thread(() -> {
+                    String response = "";
+                    while(!response.equals(null)){
+                        System.out.println(targetSocket.getInetAddress() + ": " + response);
+                        if(response.equals("***EXIT***")){
+                            disconnect();
+                            return;
+                        }
+                    }
+                }).start();
+
+                String messageOut;
+                while((messageOut = keyboard.nextLine()) != null){
+                    if(messageOut.equals("***EXIT***")){
+                        disconnect();
+                        return;
+                    }
+                    output.println(messageOut);
+                }
+        }catch(IOException ioEx){
+            ioEx.printStackTrace();
+        }
+    }
+    public static void handleChat(Socket clientSocket){
+        try(BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))){
+            String message;
+            while((message = input.readLine()) != null){
+                System.out.println(clientSocket.getInetAddress() + ": " + message);
+            }
+        }
+        catch(IOException ioEx){
+            ioEx.printStackTrace();
         }
     }
     public static void disconnect(){
